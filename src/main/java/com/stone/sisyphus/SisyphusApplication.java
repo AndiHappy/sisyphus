3bf27e464841c95ab75c6e120c4f9a256244c8f8
zhailiuzhen <zhailzh1>
2022-08-20 11:54:10 +0800

    20220820-keep happy

--- src/main/java/com/stone/sisyphus/SisyphusApplication.java
+++ src/main/java/com/stone/sisyphus/SisyphusApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SisyphusApplication {
 
     public static void main(String[] args) {
+        System.out.println("Keep Happy,boy !");
         SpringApplication.run(SisyphusApplication.class, args);
     }
 
