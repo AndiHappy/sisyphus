7ac163f2dbe8cf66a963f176c332fea8383ef2e4
zhailiuzhen <zhailzh1>
2022-08-20 11:42:18 +0800

    20220820-ini

---
+++ src/test/java/com/stone/sisyphus/SisyphusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stone.sisyphus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisyphusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
