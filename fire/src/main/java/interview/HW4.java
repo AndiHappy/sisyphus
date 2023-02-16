1f26c571b6808804815e0180797cdb9a695bce85
zhailiuzhen <zhailzh1>
2023-02-16 10:12:52 +0800

    热身

---
+++ fire/src/main/java/interview/HW4.java
@@ -0,0 +1,28 @@
+package interview;
+import java.util.Scanner;
+
+public class HW4 {
+
+    // 注意类名必须为 Main, 不要有任何 package xxx 信息
+    static class Main {
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            while (in.hasNextLine()) {
+                String line = in.nextLine();
+                if (line == null || line.startsWith(" ")) continue;
+                StringBuilder builer = new StringBuilder();
+                int i = 0;
+                while (i * 8 < line.length()) {
+                    for (int j = i * 8; j < (i + 1) * 8; j++) {
+                        char chacteri = j < line.length() ? line.charAt(j) : '0';
+                        builer.append(chacteri);
+                    }
+                    System.out.println(builer.toString());
+                    i = i + 1;
+                    builer = new StringBuilder();
+                }
+            }
+
+        }
+    }
+}
--- fire/src/main/java/interview/Main.java
+++ fire/src/main/java/interview/Main.java
@@ -6,4 +6,23 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while(in.hasNextLine()){
+            String line = in.nextLine();
+            if(line == null || line.startsWith(" ")) continue;
+            StringBuilder builer = new StringBuilder();
+            int i=0;
+            while(i*8 < line.length()){
+                for (int j = i*8; j < (i+1)*8; j++) {
+                    char chacteri = j<line.length()?line.charAt(j):'0';
+                    builer.append(chacteri);
+                }
+                System.out.println(builer.toString());
+                i=i+1;
+                builer = new StringBuilder();
+            }
+        }
+    }
+
 }
